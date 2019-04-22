50f409fdc3e1ad2d8db5766375e1ac5d40b61cd1
jessi <jessi@192.168.56.1>
2019-04-22 10:47:07 -0300

    loja virtual JNMODAS

---
+++ visao/produto/formulario.visao.php
@@ -0,0 +1,6 @@
+<form action="" method="POST">
+    nome: <input type="text" name="nome">
+    email: <input type="text" name="email">
+    senha: <input type="text" name="senha">
+    <button type="submit"> Enviar </button>
+</form>
