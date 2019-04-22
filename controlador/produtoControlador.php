393e28e56f09bf043e7405f17f9650e8be6d3af0
jessi <jessi@192.168.56.1>
2019-04-22 10:46:54 -0300

    loja virtual JNMODAS

--- controlador/produtoControlador.php
+++ controlador/produtoControlador.php
@@ -6,3 +6,18 @@ function visualizar() {
     $vetor_dados ["valor"]=  "129,90";
     exibir("produto/visualizar", $vetor_dados);
 }
+function adicionar(){
+    if (ehPost ()){
+        $nome = $_POST ["nome"];
+        $email = $_POST ["email"];
+        $senha = $_POST ["senha"];
+        
+        echo $email ;
+        
+        
+        //redirecionar("formulario/index");
+    } else {
+        exibir("produto/formulario");
+    }
+    
+}
