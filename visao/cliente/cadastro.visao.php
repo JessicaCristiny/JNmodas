8ed599df546165211d87e475483e8506b5cf36aa
1831372 <1831372@LABBMAQ09.itp.local>
2019-04-23 12:19:12 -0300

    J&Nmodas

---
+++ visao/cliente/cadastro.visao.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+    <form action="" method="POST">   
+				
+				<label for="email"><b>Nome Completo</b></label>
+				<input type="text" placeholder="Nome" name="nome" required="">
+				
+				<label for="email"><b>Email</b></label>
+				<input type="text" placeholder="Email" name="email" required="">
+				
+				<label for="email"><b>CPF</b></label>
+				<input type="text" placeholder="000.000.000-00" name="cpf" required="">
+
+				<label for="email"><b>Data de Nascimento</b></label>
+				<input type="text" placeholder="00/00/0000" name="data" required="">
+				
+				<label for="email"><b>Celular</b></label>
+				<input type="text" placeholder="(00) 0000-0000" name="cel" required="">
+			
+				<label for="psw"><b>Senha</b></label>
+				<input type="password" placeholder="Password" name="senha" required="">
+
+				<label for="psw-repeat"><b>Repita a Senha</b></label><b>
+				<input type="password" placeholder="Password" name="psw-repeat" required="">
+				
+			  <button type="submit" class="signupbtn">Confirmar</button>
+			
+		  </b>
+                     
+    </form>
+    </body>
+</html>
