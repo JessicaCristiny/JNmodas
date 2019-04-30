fd7b61499eb1a34000ed439a58673b923e7377cd
jessi <jessi@192.168.56.1>
2019-04-30 12:36:19 -0300

    loja virtual JNMODAS

--- controlador/produtoControlador.php
+++ controlador/produtoControlador.php
@@ -8,11 +8,40 @@ function visualizar() {
 }
 function adicionar(){
     if (ehPost ()){
-        $nome = $_POST ["nome"];
-        $email = $_POST ["email"];
-        $senha = $_POST ["senha"];
+        $nome = strip_tags($_POST ["nome"]);
+        $email =  strip_tags( $_POST ["email"]);
+        $senha = strip_tags($_POST ["senha"]);
         
-        echo $email ;
+    //nome
+        if (strlen(trim($_POST['nome'])) == 0) {
+            echo "Você deve inserir seu nome. <br>";
+        }
+ 
+        if(is_string($nome)){
+
+        }else{
+            echo 'Não é do tipo String.';
+        }   
+    //email    
+        if (strlen(trim($_POST['email'])) == 0) {
+            echo "Você deve inserir um e-mail. <br>";
+        }
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        if ($email == FALSE) {
+            echo  "Informe um email valido.<br>";
+        }
+    //senha
+         if (strlen(trim($_POST['senha'])) == 0) {
+            echo "Você deve inserir uma senha. <br>";
+        }
+        $senha = filter_input(INPUT_POST, 'senha', FILTER_VALIDATE_INT);
+        if ($senha == FALSE) {
+            echo  "Informe uma senha valida.<br>";
+        }
+
+        echo $nome ."<br>";
+        echo $email ."<br>";
+        echo $senha ."<br>" ;
         
         
         //redirecionar("formulario/index");
