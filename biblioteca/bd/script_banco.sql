6486a91c6b1e6c5dce029302de19a590033e9075
jessi <jessi@192.168.56.1>
2019-05-14 12:37:51 -0300

    loja virtual JNMODAS

--- biblioteca/bd/script_banco.sql
+++ biblioteca/bd/script_banco.sql
@@ -11,3 +11,13 @@ senha varchar(20) not null,
 primary key (idcliente)
 );
 
+create table enderecos (
+idendereco integer not null auto_increment,
+cidade varchar(20) not null,
+bairro varchar(20) not null,
+endereco varchar(40) not null,
+rua varchar(10) not null,
+cep varchar(20) not null,
+num varchar(20) not null,
+primary key (idendereco)
+);
