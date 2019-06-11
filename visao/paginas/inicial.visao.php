02ff2ada3e3e89457160fe0b5730f0424123da52
jessi <jessi@192.168.56.1>
2019-06-11 12:48:21 -0300

    agr vai!

--- visao/paginas/inicial.visao.php
+++ visao/paginas/inicial.visao.php
@@ -1,29 +1,6 @@
 <h1>J & N Modas</h1>
 
-<P> Nossa loja é sobre a venda de roupas estilosas para vocês!</P>
-<table BORDER ="1">
-    <thead>
-        <tr> 
-            <th>NOME</th>
-            <th>DESCRICAO</th>
-            <th>VALOR</th>
-
-        </tr>
-
-    </thead>
-    <?php foreach ($produtos as $produto): ?>
-        <tr>
-            <td><?= $produto ['idProduto'] ?></td>
-            <td><?= $produto['nome'] ?></td>
-            <td><?= $produto['valor'] ?></td>
-        </tr>
-    
-    <?php endforeach; ?>
-  
-</table>
-<h1>J & N Modas</h1>
-
-<P> Nossa loja é sobre a venda de roupas estilosas para vocês!</P>
+<P> Nossa loja apresenta roupas estilosas para você arrasar no seu look</P>
 <table BORDER ="1">
     <thead>
         <tr> 
