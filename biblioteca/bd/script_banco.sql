b533ece93a2c77a4d438d6c895071c06944e4b8e
Nathalia Silva <Nathalia Silva@10.112.70.237>
2019-06-27 17:13:41 -0300

    alterações gbd

--- biblioteca/bd/script_banco.sql
+++ biblioteca/bd/script_banco.sql
@@ -16,9 +16,14 @@ create table produto (
         nome varchar (20) not  null,
         descricao varchar (230) not null,
         valor varchar (10) not null,
-        primary key (idProduto)
-);
+        categoria varchar(8) not null,
 
+        estoqueMinimo int(11) NOT NULL,
+        estoqueMaximo int(11) NOT NULL,
+        primary key(idProduto),
+        foreign key(categoria) references categoria(codigo)
+);
+      
 create table enderecos (
 idendereco integer not null auto_increment,
 cidade varchar(20) not null,
@@ -27,6 +32,8 @@ rua varchar(100) not null,
 cep varchar(20) not null,
 num varchar(20) not null,
 primary key (idendereco)
+foreign key(idcliente) references cliente(id) on delete cascade on update cascade
+);
 );
 
 create table categoria (
@@ -35,43 +42,54 @@ descricao varchar (50) not null,
 primary key (idcategoria)
 );
 
-create table pedido (
-idpedido int (11) not null auto_increment,
-idcliente int (11) not null,
-idendereco int (11) not null,
-datacompra DATE not null,
-primary key (idcliente)
-primary key (idendereco)
+create table log_produto(
+id_Log int (11) auto_increment  not null,
+tabela varchar (45) not null,
+usuario varchar (45) not null,
+data_Hora Datetime not null,
+acao varchar (45) not null,
+dados varchar (100) not null,
+primary key (id_Log)
+);
 
+
+create table cupom(
+id int (11) auto_increment  not null,
+nome_cupom varchar (60) not null,
+desconto int (11) not null,
+primary key (id)
+);
+
+create table estoque(
+id int (11) auto_increment  not null,
+idProduto int (11) not null,
+qtde int (11) not null,
+primary key (id),
+foreign key(idProduto) references Produto(codigo) on delete cascade on update cascade
+);
+
+create table pedido (
+idPedido int (11) auto_increment  not null,
+idUsuario int(11) not null,
+idendereco int(11) not null,
+datacompra date not null,
+primary key (idPedido),
+foreign key(idusuario) references cliente (id) on delete cascade on update cascade,
+foreign key(idendereco) references endereco(idendereco) on delete cascade on update cascade
 );
 
 create table pedido_produto (
-idproduto int (11) not null,
-idpedido int (11) not null,
-quantidade int (11) not null,
-primary key (idproduto)
-primary key (idpedido)
+idproduto int(11) auto_increment not null,
+idpedido int (11)  not null,
+quantidade int(11) not null,
+foreign key(idproduto) references produto(codigo) on delete cascade on update cascade,
+foreign key(idpedido) references produto(codigo) on delete cascade on update cascade
 );
 
-create table estoque (
-idestoque int (11) not null auto_increment,
-id_produto int (11) not null,
-qtde int (11) not null,
-primary key (id_produto)
-);
 
-create table log_produto (
-id_log int (11) not null auto_increment,
-tabela varchar (45) not null,
-cliente varchar (45) not null,
-data_hora DATETIME not null,
-dados varchar (100) not null,
-primary key (id_log)
-);
 
-create table cupom (
-idcupom int (11) not null,
-nomecupom varchar (60) not null,
-desconto int (11) not null,
-primary key (idcupom)
-);
+
+
+
+
+
