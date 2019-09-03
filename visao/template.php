f8ca2846b0505a2c20bfc902817ee63d20bf181e
jessi <jessi@LAPTOP-8JVJIOAK>
2019-09-03 12:38:10 -0300

    comecei fazer o carrinho, faltou chamar a função PEGAR PRODUTO POR ID

--- visao/template.php
+++ visao/template.php
@@ -7,120 +7,120 @@
         <link rel="stylesheet" type="text/css" href="./publico/css/semantic.min.css">
         <link rel="stylesheet" href="./publico/css/app.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     </head>
     <body class="container">
         <div id="menu">
-	<div id="barrapesquisa">		
-	<div class="ui category search">
-		<div class="ui icon input">
-                    <form action="./busca/pesquisar" method="POST"> 
-                    <label for="email"><b>Pesquisar</b></label>
-                    <input type="text" placeholder="Pesquisar" name="pesquisar" required="">
-                    </form>
-		</div>
-		<div class="results"></div>
-	</div>
-	</div>
-	<div id="ladoesq">
+            <div id="barrapesquisa">		
+                <div class="ui category search">
+                    <div class="ui icon input">
+                        <form action="./busca/pesquisar" method="POST"> 
+                            <label for="email"><b>Pesquisar</b></label>
+                            <input type="text" placeholder="Pesquisar" name="pesquisar" required="">
+                        </form>
+                    </div>
+                    <div class="results"></div>
+                </div>
+            </div>
+            <div id="ladoesq">
 
-	<div class="ui animated button" tabindex="0">
-			  <div class="visible content"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Login</font></font></div>
-			  <div class="hidden content"><a href="login.html">
-				<i class="right arrow icon"></a></i>
-			  </div>
-	</div>
+                <div class="ui animated button" tabindex="0">
+                    <div class="visible content"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Login</font></font></div>
+                    <div class="hidden content"><a href="login.html">
+                            <i class="right arrow icon"></a></i>
+                    </div>
+                </div>
 
 
-	<div class="ui vertical animated button" tabindex="0">
-		  <div class="hidden content"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><a href="carrinho.html">fazer compras</font></font></a></div>
-		  <div class="visible content">
-			<i class="shop icon"></i>
-		  </div>
-	</div>
-	
-	</div>
-	<div id="logo">
-		
-	</div>
+                <div class="ui vertical animated button" tabindex="0">
+                    <div class="hidden content"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"><a href="carrinho.html">fazer compras</font></font></a></div>
+                    <div class="visible content">
+                        <i class="shop icon"></i>
+                    </div>
+                </div>
 
-<div id="barramenu">
+            </div>
+            <div id="logo">
 
-    <a href="./">Página Inicial</a>
-    <a href="./cliente/listarClientes"> Cadastro</a>
-    <a href="./produto/listarProdutos">Produtos</a>
-    <a href="./endereco/listarEnderecos">Endereços</a>
-    <a href="./categoria/listarCategoria">Categorias</a>
-    <a href="./cupom/listarCupom">Cupom</a>
-    <a href="./formaPagamento/listarFormaPagamento">Forma de Pagamento</a>
+            </div>
 
-</div>    
-</div>
+            <div id="barramenu">
 
-           
+                <a href="./">Página Inicial</a>
+                <a href="./cliente/listarClientes"> Cadastro</a>
+                <a href="./produto/listarProdutos">Produtos</a>
+                <a href="./endereco/listarEnderecos">Endereços</a>
+                <a href="./categoria/listarCategoria">Categorias</a>
+                <a href="./cupom/listarCupom">Cupom</a>
+                <a href="./formaPagamento/listarFormaPagamento">Forma de Pagamento</a>
+
+            </div>    
+        </div>
+
+
         <main class="container">
             <?php require $viewFilePath; ?>
         </main>
 
         <div class="rodape">
-		<div class="ui grid">
-			<div class="four wide column roda">
-				<h3><i class="phone icon "></i></h3>
-				<p>Cel: (15)99785-5420</p>
-			</div>
-            
-			<div class="four wide column roda">
-				<h3><i class="envelope icon"></i></h3>
-				<p>Email: jnmodas@gmail.com</p>
-			</div>
-			<div class="four wide column roda">
-				<h3><i class="credit card icon"></i></h3>
-				<p>10x sem juros no cartão</p>
-			</div>
-			<div class="four wide column roda">
-				<h3><i class="truck icon"></i></h3>
-				<p>Frete Gratis</p>
-			</div>
-		</div>
-	<div class="final">
-			
-		<div class="avaliação">
-			<div class="ui labeled button" tabindex="0">
-				<div class="ui red button">
-					<i class="heart icon"></i><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Gostei
-				</font></font></div>
-				<a class="ui basic red left pointing label"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-					1.048
-				</font></font></a>
-			</div>
-		</div>
-		<div class="redes">
-			<button class="ui circular facebook icon button">
-			  <i class="facebook icon"></i>
-			</button>
-			<button class="ui circular twitter icon button">
-			  <i class="twitter icon"></i>
-			</button>
-			<button class="ui circular linkedin icon button">
-			  <i class="linkedin icon"></i>
-			</button>
-			<button class="ui circular google plus icon button">
-			  <i class="google plus icon"></i>
-			</button>
-		</div>
-		<div class="criadoras">
-			<h1> Criado por:</h1>
-			<h3>Nathalia Silva</h3>
-			<h3>Jessica Cristiny</h3>
-		</div>
-		
-	</div>
+            <div class="ui grid">
+                <div class="four wide column roda">
+                    <h3><i class="phone icon "></i></h3>
+                    <p>Cel: (15)99785-5420</p>
+                </div>
 
-</div>
-<script
-  src="https://code.jquery.com/jquery-3.1.1.min.js"
-  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-  crossorigin="anonymous"></script>
-<script src="semantic.min.js"></script>
+                <div class="four wide column roda">
+                    <h3><i class="envelope icon"></i></h3>
+                    <p>Email: jnmodas@gmail.com</p>
+                </div>
+                <div class="four wide column roda">
+                    <h3><i class="credit card icon"></i></h3>
+                    <p>10x sem juros no cartão</p>
+                </div>
+                <div class="four wide column roda">
+                    <h3><i class="truck icon"></i></h3>
+                    <p>Frete Gratis</p>
+                </div>
+            </div>
+            <div class="final">
+
+                <div class="avaliação">
+                    <div class="ui labeled button" tabindex="0">
+                        <div class="ui red button">
+                            <i class="heart icon"></i><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> Gostei
+                            </font></font></div>
+                        <a class="ui basic red left pointing label"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
+                            1.048
+                            </font></font></a>
+                    </div>
+                </div>
+                <div class="redes">
+                    <button class="ui circular facebook icon button">
+                        <i class="facebook icon"></i>
+                    </button>
+                    <button class="ui circular twitter icon button">
+                        <i class="twitter icon"></i>
+                    </button>
+                    <button class="ui circular linkedin icon button">
+                        <i class="linkedin icon"></i>
+                    </button>
+                    <button class="ui circular google plus icon button">
+                        <i class="google plus icon"></i>
+                    </button>
+                </div>
+                <div class="criadoras">
+                    <h1> Criado por:</h1>
+                    <h3>Nathalia Silva</h3>
+                    <h3>Jessica Cristiny</h3>
+                </div>
+
+            </div>
+
+        </div>
+        <script
+            src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+        <script src="semantic.min.js"></script>
     </body>
 </html>
