bf8024f7c42a585f83a884dd723b9d3997e95bc2
jessi <jessi@192.168.56.1>
2019-10-08 12:38:14 -0300

    fiz mais um pouco do pedido

--- controlador/pedidoControlador.php
+++ controlador/pedidoControlador.php
@@ -1,24 +1,52 @@
 <?php
+
 require_once 'modelo/pedidoModelo.php';
 require_once 'modelo/formaPagamentoModelo.php';
 require_once 'modelo/enderecoModelo.php';
+require_once 'modelo/cupomModelo.php';
+require_once "modelo/ProdutoModelo.php";
 
-function salvaPedido (){
-    if (ehPost()){
-        $descricao = strip_tags($_POST ["descricao"]); 
+function salvaPedido() {
+    if (ehPost()) {
+        $descricao = strip_tags($_POST ["descricao"]);
         $dados = array();
         $msg = salvarProdutos($descricao);
         redirecionar("pedido/finalizarPedido");
-
-    }else {
+    } else {
         
     }
 }
-function finalizarPedido () {
+
+function finalizarPedido() {
+    
+    
+    if (ehPost()) {
+        $nomecupom = $_POST["nomecupom"];   
+    }
+    
     $dados = array();
     $dados["formaPagamentos"] = pegarTodasFormaPagamento();
     $dados ["enderecos"] = pegarTodosEnderecos();
-    $produtos = $_SESSION["carrinho"];
-    $dados ["h"] = $produtos;
+    $dados["cupom"] = pegarTodosCupom();
+    $IDPRODUTOS = $_SESSION["carrinho"];
+
+    
+    $vetorProdutos = array();
+    $desconto = pegarCupomPorNome($nomecupom);
+    $quant = 0;
+    $subtotal = 0;
+    foreach ($IDPRODUTOS as $IDPRODUTO) {
+        $produto = pegarProdutoPorId($IDPRODUTO);
+        $preco = $produto['valor'];
+        
+        $subtotal = $preco + $subtotal - $desconto;
+        $quant = $quant + 1;
+        $vetorProdutos[] = $produto;
+    }
+
+    $dados['subtotal'] = $subtotal;
+    $dados['quant'] = $quant;
+
+    $dados ["visaoProdutos"] = $vetorProdutos;
     exibir("pedido/finalizarPedido", $dados);
 }
--- visao/pedido/finalizarPedido.visao.php
+++ visao/pedido/finalizarPedido.visao.php
@@ -1,5 +1,25 @@
 <div class="meio">
     <form action="" method="POST">
+        <table border ="1">
+            <div class="carrinho">
+                <tr>
+                    <th>NOME</th>
+                    <th>DESCRICAO</th>
+                    <th>VALOR</th> 
+
+                </tr>
+                <?php foreach ($visaoProdutos as $visaoProduto): ?>
+                    <tr>
+                        <td><?= $visaoProduto['nome'] ?></td>
+                        <td><?= $visaoProduto['descricao'] ?></td>
+                        <td><?= $visaoProduto['valor'] ?></td>
+
+                    </tr>
+                <?php endforeach; ?>
+                    
+                <h4>Subtotal: <?=$subtotal?></h4>
+                <p><?=$quant?> Produtos</p>
+        </table>
         <select name="forma">
             <option value="default">Selecione uma Forma de Pagamento</option>
             <?php foreach ($formaPagamentos as $formaPagamento): ?>
@@ -13,4 +33,11 @@
                 <option value="<?= $endereco["idendereco"] ?>"><?= $endereco["rua"] ?></option>
             <?php endforeach; ?>
         </select>
+
+        Nome do Cupom: <input type="text" name="nomecupom" value="<?= @$cupom['nomecupom'] ?>"><br>
+        <button type="submit"><a href="./pedido/finalizarPedido">Aplicar Desconto</a></button>   
+
+        <button type="submit" class="signupbtn">Finalizar Pedido</button>
+
+    </form>
 </div>
