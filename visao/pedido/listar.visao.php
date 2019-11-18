f82cb334cd86acd86202bf4cead2ef2939581e14
jessi <jessi@192.168.56.1>
2019-11-18 11:43:07 -0200

    quase terminei o PEDIDO

--- visao/pedido/listar.visao.php
+++ visao/pedido/listar.visao.php
@@ -1,24 +1,22 @@
 <div class="meio">
-<h2></h2>
-        
-     
-  
-            <table border ="1">
-                <tr>
+<h2>Lista de Pedidos</h2>
+
+<table border ="1">
+ <tr>
                    
                     <th>PEDIDO</th>
                     <th>ENDERECO</th>
                     <th>FORMA DE PAGAMENTO</th>
                    	
                 </tr>
-            </thead>
+         
            
             <?php foreach ($pedidos as $pedido): ?>
             <tr>
                         <td><?=$pedido['idusuario']?></td>
                         <td><?=$pedido['idendereco']?></td>
                         <td><?=$pedido['descricao']?></td>
-				
+    
                               
             </tr>
             
@@ -27,3 +25,4 @@
             <a href= ".pedido/pegarPedido" class ="btn btn-primary"> Novo Pedido</a>
             <?php } ?>
 
+</div>
